8a8a4929b8304a9e086a263f15e1d3d00025bf1a
Giulia <giu.olivers@gmail.com>
2021-12-16 19:00:10 -0300

    Initial commit

---
+++ src/main/java/com/serasa/desafiorestapi/DesafiorestapiApplication.java
@@ -0,0 +1,13 @@
+package com.serasa.desafiorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiorestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiorestapiApplication.class, args);
+	}
+
+}
